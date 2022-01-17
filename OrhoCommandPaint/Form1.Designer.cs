08f20aa59a5ae385a3a6602ab310f6d045d4ccb5
arash gh <arashgh68@outlook.com>
2022-01-17 15:47:13 +0330

    Add project files.

---
+++ OrhoCommandPaint/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace OrhoCommandPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawPanel = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.drawPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawPanel
+            // 
+            this.drawPanel.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.drawPanel.Location = new System.Drawing.Point(12, 55);
+            this.drawPanel.Name = "drawPanel";
+            this.drawPanel.Size = new System.Drawing.Size(776, 383);
+            this.drawPanel.TabIndex = 0;
+            this.drawPanel.TabStop = false;
+            this.drawPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawPanel_MouseDown);
+            this.drawPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawPanel_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Click in bellow panel and draw line";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drawPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.drawPanel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox drawPanel;
+        private Label label1;
+    }
+}
---
+++ OrhoCommandPaint/Form1.cs
@@ -0,0 +1,147 @@
+﻿namespace OrhoCommandPaint
+{
+    public partial class Form1 : Form
+    {
+        int? initGeneralX = null;
+        int? initGeneralY = null;
+        bool startPaint = false;
+
+        List<Line> lines = new List<Line>();
+
+        Bitmap bitmap;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+        private void removePreviewLines()
+        {
+            foreach (var item in lines.Where(l => l.preview == true).ToList())
+            {
+                lines.Remove(item);
+            }
+        }
+        private Line drawOrthoLine(int startX, int startY, int endX, int endY, Color color)
+        {
+            int diffY = endY - startY;
+            int diffX = endX - startX;
+            double ratio = 0;
+
+            if (Math.Abs(diffY) > Math.Abs(diffX))
+            {
+                ratio = Math.Abs((double)diffX / diffY);
+                //اگر خط عمود رو به پایین باشد
+                if (ratio < 0.35)
+                {
+                    endX = startX;
+                }
+                else
+                {
+                    endX = startX + (diffX < 0 ? Math.Abs(diffY) * -1 : Math.Abs(diffY));
+                    endY = startY + diffY;
+                }
+            }
+            else
+            {
+                ratio = Math.Abs((double)diffY / diffX);
+                if (ratio < 0.35)
+                {
+                    endY = startY;
+                }
+                else
+                {
+                    endX = startX + (diffX < 0 ? Math.Abs(diffY) * -1 : Math.Abs(diffY));
+                    endY = startY + diffY;
+                }
+            }
+
+            return new Line
+            {
+                startX = startX,
+                startY = startY,
+                endX = endX,
+                endY = endY,
+                color = color,
+            };
+
+        }
+
+        private void UpdateDrawPanel()
+        {
+            bitmap = new Bitmap(drawPanel.Width, drawPanel.Height);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.Clear(Color.White);
+                foreach (var line in lines)
+                {
+                    Pen p = new Pen(line.color, 4);
+                    g.DrawLine(p, new Point(line.startX, line.startY), new Point(line.endX, line.endY));
+                }
+            }
+            drawPanel.Image = bitmap;
+        }
+
+        private void drawPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (startPaint)
+            {
+                int startX;
+                int startY;
+                int endY = e.Y;
+                int endX = e.X;
+                startX = initGeneralX.Value;
+                startY = initGeneralY.Value;
+
+                var line = drawOrthoLine(startX, startY, endX, endY, Color.Black);
+
+
+                initGeneralX = line.endX;
+                initGeneralY = line.endY;
+
+
+                lines.Add(line);
+            }
+            else
+            {
+                startPaint = true;
+
+                initGeneralX = e.X;
+                initGeneralY = e.Y;
+            }
+            UpdateDrawPanel();
+        }
+
+        private void drawPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (startPaint)
+            {
+                removePreviewLines();
+                int startX;
+                int startY;
+                int endY = e.Y;
+                int endX = e.X;
+                startX = initGeneralX.Value;
+                startY = initGeneralY.Value;
+
+                var line = drawOrthoLine(startX, startY, endX, endY, Color.Blue);
+                line.preview = true;
+                lines.Add(line);
+            }
+            UpdateDrawPanel();
+        }
+    }
+    public class Line
+    {
+        public int startX;
+        public int startY;
+        public int endX;
+        public int endY;
+        public Color color;
+        public bool preview = false;
+    }
+}
